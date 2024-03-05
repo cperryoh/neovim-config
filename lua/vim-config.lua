f0c7fdbb3707b6b61d24a514fdcf63dffe9bb08b
Cole <perryca@rose-hulman.edu>
2024-03-05 09:51:33 -0500

    Change config

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -1,5 +1,9 @@
 vim.cmd("set expandtab")
+vim.cmd("set number")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("nmap < v<")
+vim.cmd("nmap > v>")
+vim.cmd("imap jk <Esc>")
 vim.g.mapleader = " "
