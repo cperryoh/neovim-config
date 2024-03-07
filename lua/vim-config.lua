f543956fcb41031bb0ca168d6bb05754bbf65a20
Cole <perryca@rose-hulman.edu>
2024-03-07 09:27:23 -0500

    Neotree config

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -9,6 +9,7 @@ vim.cmd("imap jk <Esc>")
 vim.g.mapleader = " "
 vim.cmd("nmap <leader>wq :wq<CR>")
 vim.cmd("nmap <Esc><Esc> :qa<CR>")
+vim.cmd("nmap <leader><CR> <C-w>l")
 function man(args)
   os.execute("man "+args[0])
 end
