4f2419e46904b3232cc87d6c5ca77d7714203c03
Cole <cperryoh@gmail.com>
2024-03-11 22:26:34 -0400

    Add formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -20,6 +20,7 @@ return {
 			lua = { "stylua" },
 			python = { "isort", "black" },
 			javascript = { { "prettierd", "prettier" } },
+      c={"clang_format"}
 		},
 		-- Set up format-on-save
 		format_on_save = { timeout_ms = 500, lsp_fallback = true },
