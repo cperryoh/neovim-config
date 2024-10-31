9143609f69d56bbae39758679d05c9de9d14c434
Cole Perry <cperryoh@gmail.com>
2024-10-31 16:07:51 -0400

    Add bind for neovim close

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,22 +7,22 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>", {})
+		vim.keymap.set("n", "<leader>nc", ":Neotree close<CR>", {})
+		vim.keymap.set("n", "<leader>nn", ":Neotree filesystem reveal right<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-    require("neo-tree").setup({
-      event_handlers = {
+		require("neo-tree").setup({
+			event_handlers = {
 
-          {
-            event = "file_opened",
-            handler = function(file_path)
-              -- auto close
-              -- vimc.cmd("Neotree close")
-              -- OR
-              require("neo-tree.command").execute({ action = "close" })
-            end
-          },
-
-        }
-    })
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
 	end,
 }
