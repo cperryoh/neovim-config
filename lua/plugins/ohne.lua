7a7ddb2d359d7949bb9360d8a6d8a4470912bdf9
Cole Perry <cperryoh@gmail.com>
2024-11-23 15:09:22 -0500

    Backup commit

--- lua/plugins/ohne.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'blumaa/ohne-accidents',
-    config = function()
-      require("ohne-accidents").setup()
-      vim.api.nvim_set_keymap('n', '<leader>oh', ':OhneAccidents<CR>', {noremap = true, silent = true})
-    end
-  },
-}
