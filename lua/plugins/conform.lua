6bfbb75f5e3e05572ad929975b17e76e927f6c78
Cole <cperryoh@gmail.com>
2024-12-02 12:29:05 -0500

    Add css formating

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				css = { "rustywind" },
 				-- Conform will run multiple formatters sequentially
 				python = { "isort", "black" },
 				java = { "google_java" },
