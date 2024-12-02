8a5ad6c0321bcc1d470a2ad4a38513fdfaf2c25a
Cole <cperryoh@gmail.com>
2024-12-02 12:29:54 -0500

    Fix config options

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,22 +1,23 @@
 return {
 	{
 		"windwp/nvim-ts-autotag",
-		opts = {
-			-- Defaults
-			enable_close = true, -- Auto close tags
-			enable_rename = true, -- Auto rename pairs of tags
-			enable_close_on_slash = true, -- Auto close on trailing </
-		},
-		-- Also override individual filetype configs, these take priority.
-		-- Empty by default, useful if one of the "opts" global settings
-		-- doesn't work well in a specific filetype
-		per_filetype = {
-			["typescript"] = {
-				enable_close = true,
-			},
-			["html"] = {
-				enable_close = true,
-			},
-		},
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+				-- Also override individual filetype configs, these take priority.
+				-- Empty by default, useful if one of the "opts" global settings
+				-- doesn't work well in a specific filetype
+				per_filetype = {
+					["html"] = {
+						enable_close = true,
+					},
+				},
+			})
+		end,
 	},
 }
